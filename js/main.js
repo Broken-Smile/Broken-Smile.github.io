8e4371805a89718d291457aa0969577985d12dde
SebastianOjeda <sebastianojeda.school@gmail.com>
2015-03-04 20:36:10 -0800

    fixing design and color

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
-
+//parallax code
 
 $(window).ready( function() { 
 
   $(window).stellar();
 
+});
+
+//Smooth Scrolling
+
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 2000);
+    return false;
 });
